2d55ebc82071162bb7102bb9c2dc4441332b2dae
Juan carlos chocllu <j.chocllu@opti-system.com>
2026-02-02 14:33:59 -0400

    cambios

--- src/module/cliente/router/clienteRouter.go
+++ src/module/cliente/router/clienteRouter.go
@@ -12,6 +12,6 @@ func NewClienteRouter(mux *http.ServeMux, controller *controller.ClienteControll
 	mux.Handle("GET /api/cliente", rutaProtegida(http.HandlerFunc(controller.ListarClientesController)))
 	mux.Handle("POST /api/cliente", rutaProtegida(http.HandlerFunc(controller.CrearClienteController)))
 	mux.Handle("DELETE /api/cliente/{id}", rutaProtegida(http.HandlerFunc(controller.EliminarClienteController)))
-	mux.Handle("PATCH /api/cliente/{id}", rutaProtegida(http.HandlerFunc(controller.EliminarClienteController)))
+	mux.Handle("PATCH /api/cliente/{id}", rutaProtegida(http.HandlerFunc(controller.ActualizarClienteController)))
 
 }
